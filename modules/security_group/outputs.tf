bf63f69997970dca6f4926cc842402d4ea9ad800
Ali Naveed <114981749+alinaveed1983@users.noreply.github.com>
2025-04-16 20:01:57 +0530

    Add files via upload

---
+++ modules/security_group/outputs.tf
@@ -0,0 +1,6 @@
+# ================================
+# modules/security_group/outputs.tf
+# ================================
+output "security_group_id" {
+  value = aws_security_group.this.id
+}
---
+++ modules/security_group/variable.tf
@@ -0,0 +1,30 @@
+# ================================
+# modules/security_group/variables.tf
+# ================================
+variable "name" {}
+variable "description" {}
+variable "vpc_id" {}
+
+variable "ingress_rules" {
+  type = list(object({
+    from_port       = number
+    to_port         = number
+    protocol        = string
+    cidr_blocks     = optional(list(string))
+    security_groups = optional(list(string))
+  }))
+}
+
+variable "egress_rules" {
+  type = list(object({
+    from_port   = number
+    to_port     = number
+    protocol    = string
+    cidr_blocks = list(string)
+  }))
+}
+
+variable "tags" {
+  type    = map(string)
+  default = {}
+}
